3a6de8cb612b0829a17a0cbc33f2d9b24c328ea2
Elelia04 <148786438+Elelia04@users.noreply.github.com>
2025-11-14 16:09:45 +0100

    Update data_loading.py

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,1 +1,65 @@
+import json
+import pandas as pd
+import os
+
+# --- Define the path to our data ---
+COMPETITION_NAME = 'fds-pokemon-battles-prediction-2025'
+DATA_PATH = os.path.join('../input', COMPETITION_NAME)
+
+train_file_path = os.path.join(DATA_PATH, 'train.jsonl')
+test_file_path = os.path.join(DATA_PATH, 'test.jsonl')
+train_data = []
+
+#LOADING TRAIN 
+# Read the file line by line
+print(f"Loading data from '{train_file_path}'...")
+try:
+    with open(train_file_path, 'r') as f:
+        for line in f:
+            # json.loads() parses one line (one JSON object) into a Python dictionary
+            train_data.append(json.loads(line))
+
+    print(f"Successfully loaded {len(train_data)} battles.")
+
+    # Let's inspect the first battle to see its structure
+    print("\n--- Structure of the first train battle: ---")
+    if train_data:
+        first_battle = train_data[0]
+        
+        # To keep the output clean, we can create a copy and truncate the timeline
+        battle_for_display = first_battle.copy()
+        battle_for_display['battle_timeline'] = battle_for_display.get('battle_timeline', [])[:2] # Show first 2 turns
+        
+        # Use json.dumps for pretty-printing the dictionary
+        print(json.dumps(battle_for_display, indent=4))
+        if len(first_battle.get('battle_timeline', [])) > 3:
+            print("    ...")
+            print("    (battle_timeline has been truncated for display)")
+
+
+except FileNotFoundError:
+    print(f"ERROR: Could not find the training file at '{train_file_path}'.")
+    print("Please make sure you have added the competition data to this notebook.")
+
+
+
+#LOADING TEST
+test_data = []
+
+print(f"\nLoading test data from '{test_file_path}'...")
+try:
+    with open(test_file_path, 'r') as f:
+        for line in f:
+            test_data.append(json.loads(line))
+    print(f"Loaded {len(test_data)} test battles.")
+
+except FileNotFoundError:
+    print(f"ERROR: test file not found at '{test_file_path}'")
+
+
+print("\n--- First train battle structure:---")
+if train_data:
+    first_battle = dict(train_data[0])
+    first_battle["battle_timeline"] = first_battle.get("battle_timeline", [])[:2]
+    print(json.dumps(first_battle, indent=4))
 
